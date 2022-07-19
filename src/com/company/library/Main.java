8f522b882f87bf07ae2f4402f81dd311257d20d8
dercykar <cykaru@yandex.ru>
2022-07-20 01:41:18 +0300

    first

---
+++ src/com/company/library/Main.java
@@ -0,0 +1,25 @@
+package com.company.library;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //-----
+        //использование старой версии Калькулятора:
+        Calculator calc = new Calculator();
+        System.out.println(
+                calc.newFormula()
+                        .addOperand(5)
+                        .addOperand(15)
+                        .calculate(Calculator.Operation.MULT)
+                        .result()
+        );
+        //-----
+
+        //НОВЫЙ ВАРИАНТ использования:
+        Ints intsCalc = new IntsCalculator();
+        System.out.println(intsCalc.sum(2, 2));
+        System.out.println(intsCalc.mult(10, 22));
+        System.out.println(intsCalc.pow(2, 10));
+
+    }//endofmain
+}
